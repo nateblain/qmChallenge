5714cecd4ae4d0c2d35b131d70c9726020878844
Nate Blain <blain89@gmail.com>
2016-08-09 15:17:46 -0700

    [chore] add comment, remove unnecessary logic

--- public/components/square.js
+++ public/components/square.js
@@ -1,11 +1,16 @@
+/*=========================================
+Square Function-based Component: handles
+square rendered in the SVG component if
+width, height, and color props exist
+=========================================*/
+
 import React, { Component } from 'react';
 
 const Square = (props) => {
-  let xPos = Math.round((Math.random()*(590 - props.squareWidth)) + (props.squareWidth + 10));
-  let yPos = Math.round((Math.random()*(390 - props.squareHeight)) + (props.squareHeight + 10));
-  if (!props.squareWidth) {
-    return <square></square>;
-  }
+  console.log(props);
+  const xPos = Math.round((Math.random()*(590 - props.squareWidth)) + (props.squareWidth + 10));
+  const yPos = Math.round((Math.random()*(390 - props.squareHeight)) + (props.squareHeight + 10));
+
   return (
     <rect x={xPos} y={yPos} width={props.squareWidth} height={props.squareHeight} fill={props.squareColor}></rect>
   );
